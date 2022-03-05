df5f0de67bf3b0dc7d4ad74996c7bd995edc322f
Bondhan Mondal <bondhan2725@gmail.com>
2022-03-05 22:36:10 +0600

    29-1 String comparison using toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,12 @@
+const anthem = 'Amar Sonar Bangla Ami Tomay Valobashi';
+
+const search = 'valo';
+
+const userInput = 'blackpinK';
+const savedUserName = 'blackPink';
+
+if (userInput.toLocaleLowerCase() == savedUserName.toLocaleLowerCase()){
+    console.log('user exists');
+}
+
+// variable.toUpperCase() -- will convert string characters to capital letters
